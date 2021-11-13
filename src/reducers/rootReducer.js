ae3a10be709b79f295bb6c0ed55373615beb1f78
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-11-13 16:43:56 -0500

    wrote basic reducer actions for loading/adding stores and user location

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,3 +1,36 @@
-const rootReducer = state => {
-  return state
+const initialState = {
+  stores: [],
+  location: '',
+  requesting: false
+}
+
+const rootReducer = (state, action) => {
+  switch(action.type) {
+    case 'LOADING_STORES':
+      return {
+        ...state,
+        stores: [...state.stores],
+        requesting: true
+      }
+    case 'ADD_STORES':
+      return {
+        ...state,
+        stores: action.stores,
+        requesting: false
+      }
+    case 'LOADING_LOCATION':
+      return {
+        ...state,
+        location: state.location,
+        requesting: true
+      }
+    case 'ADD_LOCATION':
+      return {
+        ...state,
+        location: action.location,
+        requesting: false
+      }
+    default:
+      return state
+  }
 }
