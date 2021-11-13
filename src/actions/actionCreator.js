10388bcfb1c79879313deb16db4b053fbdfc1c07
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-11-13 15:39:41 -0500

    stubbed out component skeleton and basic actions

---
+++ src/actions/actionCreator.js
@@ -0,0 +1,8 @@
+const getStores = () => {
+  // reach out to Baltimore City Grocery Stores Dashboard for stores
+  // URL https://opendata.arcgis.com/datasets/a49b298a1cb9411fb423a76623445b97_0.geojson
+}
+
+const getLocation = () => {
+  // get user's location, not sure how this works yet
+}
