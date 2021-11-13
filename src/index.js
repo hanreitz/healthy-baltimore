55d6cdba216ecf835f48c5b6e497b2b287ce5cff
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-11-13 16:39:06 -0500

    adds Provider and store to index.js for redux/thunk setup

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
 
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
